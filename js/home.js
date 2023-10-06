f480412adb685b0d7a5349437f0900946f2db991
Hiago <hiago@gmail.com>
2023-10-06 18:17:55 -0300

    Adicionando mapa e seção de redes sociais

---
+++ js/home.js
@@ -0,0 +1,15 @@
+function FatecM(){
+    console.log("Fatec de Mauá")
+}
+
+function FatecSA(){
+    console.log("Fatec de Santo André")
+}
+
+function FatecSC(){
+    console.log("Fatec de São Caetano")
+}
+
+function FatecSP(){
+    console.log("Fatec de São Paulo")
+}
