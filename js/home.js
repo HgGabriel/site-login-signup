d5135c2d407c021470f1e0977e4910771f7531de
Hiago <hiago@gmail.com>
2023-10-07 01:44:01 -0300

    projeto quase finalizado, atribuído telas individuais, implementado mapa e links para redes sociais estão personalizados.

--- js/home.js
+++ js/home.js
@@ -1,15 +1,26 @@
 function FatecM(){
     console.log("Fatec de Mauá")
+    window.open("http://fatecmaua.com.br", "content__frame__site")
+    document.querySelector("#iframeMap").src = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3654.3453341853724!2d-46.460124799999996!3d-23.6636053!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce69426dbe06a3%3A0xf0b5c494be47ae58!2sFaculdade%20de%20Tecnologia%20de%20Mau%C3%A1!5e0!3m2!1sen!2sbr!4v1696562560942!5m2!1sen!2sbr"
+    document.querySelector("#link__instagram").href = "https://www.instagram.com/fatecmauaoficial/"
+    document.querySelector("#link__facebook").href = "https://www.facebook.com/fatecmauaoficial"
+    document.querySelector("#link__linkedin").href = "https://www.linkedin.com/school/fatecmaua/"
 }
 
 function FatecSA(){
     console.log("Fatec de Santo André")
+    window.open("http://fatecsantoandre.edu.br/", "content__frame__site")
+    document.querySelector("#iframeMap").src = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d58471.20834845204!2d-46.54122935944926!3d-23.659841505207787!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce428eaaa4ea8d%3A0xd8295253f43edd35!2sFatec%20Santo%20Andr%C3%A9!5e0!3m2!1sen!2sbr!4v1696651417593!5m2!1sen!2sbr" 
 }
 
 function FatecSC(){
     console.log("Fatec de São Caetano")
+    window.open("http://www.fatecsaocaetano.edu.br/", "content__frame__site")
+    document.querySelector("iframeMap").src = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d116947.78342470211!2d-46.60531858460191!3d-23.653839373936247!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce5cb6c8baf5a7%3A0x24fb537795849cc!2sFaculty%20of%20Technology%20of%20S%C3%A3o%20Caetano%20do%20Sul!5e0!3m2!1sen!2sbr!4v1696651589601!5m2!1sen!2sbr"
 }
 
 function FatecSP(){
     console.log("Fatec de São Paulo")
+    window.open("http://www.fatecsp.br/", "content__frame__site")
+    document.querySelector("iframeMap").src = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d234116.8093769951!2d-46.632205991729265!3d-23.529799473564115!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce585f8b81eb75%3A0xb742158dee17d684!2sS%C3%A3o%20Paulo%20State%20Technological%20College!5e0!3m2!1sen!2sbr!4v1696651705567!5m2!1sen!2sbr"
 }
